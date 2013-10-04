90d0d2ec57dd12f2b96fd5695a7b4bcc8e93a1a3
Tomfool <xji3@ncsu.edu>
2013-10-04 17:10:22 -0400

    before sync

--- XiangDraftSubProcess.py
+++ XiangDraftSubProcess.py
@@ -1,5 +1,8 @@
-from protein import *
-from data_test import *
+#import itertools, numpy, string, subprocess, re
+#from __future__ import division
+import subprocess
+from collections import defaultdict
+import os.path, gzip
 import tempfile
 import os
 
@@ -18,58 +21,22 @@ import os
 ##                        '-bsequence=asis:' + ccds_AA
 ##                    ]).split('\n')
 
-msms_localfile_dir='/Users/xji3/clemensCode/aux'
-if os.path.isfile(msms_localfile_dir+'/msms'):
-    MSMS = msms_localfile_dir+'/msms'
+pdb_file='/Users/xji3/Documents/Naccess/4A14.pdb.gz'
+
+naccess_localfile_dir='/Users/xji3/Documents/Naccess'
+if os.path.isfile(naccess_localfile_dir+'/naccess'):
+    naccess=naccess_localfile_dir+'/naccess'
 else:
-    MSMS = 'msms'
+    naccess= 'naccess'
 
+print subprocess.check_output([
+    naccess,
+    pdb_file
+    ],cwd=naccess_localfile_dir+'/')
 
-PDB_TO_XYZR="./aux/pdb_to_xyzr"
-#pdb_file='/Users/xji3/clemensCode/newDataOutput/pdb/1CS8/1CS8.pdb.gz'
-pdb_file='/Users/xji3/clemensCode/tmp/4A14.pdb'
-
-#xyz_tmp=tempfile.mktemp()
-if not '/Users/xji3/clemensCode/tmp/':
-    tmp_dir=os.mkdir('/Users/xji3/clemensCode/tmp/')
-else:
-    tmp_dir='/Users/xji3/clemensCode/tmp/'
-xyz_tmp=tempfile.mktemp(dir=tmp_dir)+'.xyzr'
-PDB_TO_XYZR=PDB_TO_XYZR+" %s > %s"
-make_xyz=PDB_TO_XYZR % (pdb_file, xyz_tmp)
-if os.system(make_xyz):
-    terminate=True
-else:
-    os.system(make_xyz)
-
-##subprocess.check_output([
-##    PDB_TO_XYZR,
-##    pdb_file,
-##    'a.xyzr'
-##    ])
-
-surface_tmp=tempfile.mktemp(dir=tmp_dir)
-MSMS=MSMS+" -probe_radius 1.5 -if %s -of %s > "+tempfile.mktemp(dir=tmp_dir)
-make_surface=MSMS % (xyz_tmp, surface_tmp)
-print make_surface
-
-if os.system(make_surface):
-    terminate=True
-else:
-    os.system(make_surface)
-
-surface_file=surface_tmp+'.vert'
-
-fp=open(surface_file, "rb")
-vertex_list=[]
-for l in fp.readlines():
-    sl=l.split()
-#cl: length is 10
-#        if not len(sl)==9:
-    if not len(sl)==10:
-        # skip header
-        continue
-    vl=map(float, sl[0:3])
-    vertex_list.append(vl)
-fp.close()
-
+#subprocess.Popen(naccess,cwd=r'./pdb')
+##with open('./pdb','w') as outfile:
+##    subprocess.check_output([
+##        naccess,
+##        pdb_file
+##        ])
